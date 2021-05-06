b71e0d479555633a5fc6f829ea03db7013381b8a
HannesPreishuber <hannesp@ppedv.de>
2021-05-06 11:21:50 +0200

    Routing mit Parameter

---
+++ BlazorTrainingCGI2/Pages/modul2/franz.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingCGI2.Pages.modul2
+{
+    public class franzModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
