72de1fe2e201e1544766d32af241e67870bd5dd6
HannesPreishuber <hannesp@ppedv.de>
2021-05-06 16:42:30 +0200

    HttpClient einfacher Wert

--- BlazorTrainingCGI2/Startup.cs
+++ BlazorTrainingCGI2/Startup.cs
@@ -31,7 +31,7 @@ namespace BlazorTrainingCGI2
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<ChatVM>();
-
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
