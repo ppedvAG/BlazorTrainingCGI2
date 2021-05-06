dd7db05485e8f6f8d97d9a47abc71460825013ab
HannesPreishuber <hannesp@ppedv.de>
2021-05-06 16:27:14 +0200

    Datum desaster

--- BlazorTrainingCGI2/Startup.cs
+++ BlazorTrainingCGI2/Startup.cs
@@ -31,6 +31,7 @@ namespace BlazorTrainingCGI2
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<ChatVM>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
