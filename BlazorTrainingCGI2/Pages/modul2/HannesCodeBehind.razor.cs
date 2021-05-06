f121cf5750925d9331ebadf565a64f9688c141ea
HannesPreishuber <hannesp@ppedv.de>
2021-05-06 11:09:10 +0200

    Coding Basics

---
+++ BlazorTrainingCGI2/Pages/modul2/HannesCodeBehind.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTrainingCGI2.Pages.modul2
+{
+    public partial class HannesCodeBehind
+    {
+    }
+}
