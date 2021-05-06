bd39f0151e3b66308f7295a6b5e413db33954510
HannesPreishuber <hannesp@ppedv.de>
2021-05-06 16:05:59 +0200

    Chat mitz Viewmodel

---
+++ BlazorTrainingCGI2/Pages/modul4/ChatVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTrainingCGI2.Pages.modul4
+{
+    public class ChatVM
+    {
+        public Action onAdd;
+
+        public List<string> ChatListe { get; set; } = new List<string>();
+
+        public void Add(string msg)
+        {
+            ChatListe.Add(msg);
+            onAdd?.Invoke();
+        }
+    }
+}
--- BlazorTrainingCGI2/Startup.cs
+++ BlazorTrainingCGI2/Startup.cs
@@ -1,4 +1,5 @@
 using BlazorTrainingCGI2.Data;
+using BlazorTrainingCGI2.Pages.modul4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace BlazorTrainingCGI2
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ChatVM>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
