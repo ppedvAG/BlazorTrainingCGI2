03a39437bc702e2687d026d6e27c9e65ae3996d6
HannesPreishuber <hannesp@ppedv.de>
2021-05-07 10:07:45 +0200

    Boostrap designed Error Messages

---
+++ BlazorTrainingCGI2/Pages/modul5/BootstrapCSS.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTrainingCGI2.Pages.modul5
+{
+    public class BootstrapCSS : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+            return isValid ? "is-valid" : "is-invalid";
+        }
+    }
+}
---
+++ BlazorTrainingCGI2/Pages/modul5/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTrainingCGI2.Pages.modul5
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="da muss was rein")]
+        [Range(minimum:150,maximum:200,ErrorMessage ="Keine Achterbahn")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [StringLength( maximumLength:20, ErrorMessage = "zu lange")]
+        public string Name { get; set; }
+        public DateTime GebDat { get; set; }
+    }
+}
